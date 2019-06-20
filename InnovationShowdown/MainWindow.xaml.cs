3a88eeccb86f92f132d3a07f3468ec2b415b046a
Tyler Berskhire <Tyler.Berkshire@hyland.com>
2019-06-20 13:11:02 -0400

    Added main functionality, uses cmd args to trigger text boxes

--- InnovationShowdown/MainWindow.xaml.cs
+++ InnovationShowdown/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,33 @@ namespace InnovationShowdown
         public MainWindow()
         {
             InitializeComponent();
+
+            singleKnock.Visibility = Visibility.Hidden;
+            doubleKnock.Visibility = Visibility.Hidden;
+            heldKnock.Visibility = Visibility.Hidden;
+
+            string[] args = Environment.GetCommandLineArgs();
+
+            if (args.Length == 2)
+            {
+                switch (args[1])
+                {
+                    case "1":
+                        singleKnock.Visibility = Visibility.Visible;
+                        singleKnock.Text = "";
+                        break;
+                    case "2":
+                        doubleKnock.Visibility = Visibility.Visible;
+                        doubleKnock.Text = "";
+                        break;
+                    case "3":
+                        heldKnock.Visibility = Visibility.Visible;
+                        heldKnock.Text = "";
+                        break;
+                }
+
+                // Make window close
+            }
         }
     }
 }
